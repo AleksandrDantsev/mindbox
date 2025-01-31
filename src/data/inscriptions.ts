44a6e78cd8b2a82c09d346225e931be02715cb26
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-01-31 10:40:27 +0300

    [mb-2] Подписи для кнопок

---
+++ src/data/inscriptions.ts
@@ -0,0 +1,8 @@
+export const t = {
+    tooltip: {
+        deleteCard: "Delete the task",
+        showInfo: "Show task information",
+        moveUp: "Move higher",
+        moveDown: "Move below",
+    }
+};
