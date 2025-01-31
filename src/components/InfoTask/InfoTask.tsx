ead8f21d326b87f50bbbd401f4e77031dd36882e
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-01-31 10:37:25 +0300

    [mb-2] Drawer для отображения информации

---
+++ src/components/InfoTask/InfoTask.tsx
@@ -0,0 +1,34 @@
+import stl from "./InfoTaskDrawer.module.scss";
+import { Drawer } from 'antd';
+
+interface IInfoTaskDrawer {
+    title: string;
+    isOpened: boolean;
+    children: React.ReactNode;
+    extra?: React.ReactNode;
+    onClose?: () => void;
+    size?: "default" | "large";
+    placement?: 'left' | 'right' | 'top' | 'bottom';
+};
+
+const InfoTaskDrawer: React.FC<IInfoTaskDrawer> = (
+    { title, size, isOpened, onClose, children, extra, placement }
+    ) => {
+
+    return (
+        <div className={stl.drawer_info_task}>
+            <Drawer
+                title={title}
+                placement={placement || "right"}
+                size={size}
+                onClose={onClose}
+                open={isOpened}
+                extra={extra || <></>}
+            >
+                {children}
+            </Drawer>
+        </div>
+    )
+}
+
+export default InfoTaskDrawer;
