d82ff1c67abd0c95fd4cd37d8f3d4763602e7c20
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-01 15:08:39 +0300

    [mb-3] actions

--- src/pages/TodoList/TodoList.tsx
+++ src/pages/TodoList/TodoList.tsx
@@ -1,77 +1,91 @@
-import { useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 import stl from "./TodoList.module.scss";
-import { Input, Layout, Switch } from 'antd';
-import { Icon } from "@ricons/utils"; 
-import { AddAlt, Search as ISearch } from "@ricons/carbon";
 import TaskCard from "../../components/TaskCard/TaskCard";
+import { Input, Layout, Switch } from 'antd';
+import { AddAlt, Search as ISearch } from "@ricons/carbon";
+import { Icon } from "@ricons/utils"; 
 import { tasks } from "../../data/tasks";
 import { ITask } from "../../types/TTasks";
-import PanelActionsTasks from "../../components/PanelActionsTasks/PanelActionsTasks";
 import { action } from "../../helpers/taskActions";
+import { TTypeCompletedTask } from "../../types/ChangeDataAction";
 import { capitalize } from "../../utils/capitilize";
+import PanelActionsTasks from "../../components/PanelActionsTasks/PanelActionsTasks";
+import NotFound from "../../components/NotFound/NotFound";
 
 const { Content } = Layout;
 const { Search } = Input;
 
 const payloadInput = {
     add: {
-        placeholder: "Add",
+        placeholder: "Add a task",
         icon: 
-            <Icon size={22} color={"#fffff"}>
+            <Icon size={19} color={"#fffff"}>
                 <AddAlt />
-            </Icon>,
+            </Icon>
     },
     search: {
-        placeholder: "Search",
+        placeholder: "Search a task",
         icon: 
-        <Icon size={22} color={"#fffff"}>
-                <ISearch />
-            </Icon>,
-    },
+            <Icon size={19} color={"#fffff"}>
+                    <ISearch />
+            </Icon>
+    }
 };
 
-const currentDay = "16march2024";
+const currentDay = "16march2024"; // Временно
 
 
 const TodoList: React.FC = () => {
     const [data, setData] = useState<ITask[]>(tasks[currentDay]);
-    const [actionInput, setActionInput] = useState("add");
+    const [actionInput, setActionInput] = useState<"search" | "add">("add");
     const [inputValue, setInputValue] = useState<string>("");
+    const [typeTasks, setTypeTasks] = useState<TTypeCompletedTask>("All");
 
     const currentInputPayload = payloadInput[actionInput as keyof typeof payloadInput];
-    console.log(data)
-    const changeActionInput = (checked: boolean) => {
-        setActionInput(checked ? "search" : "add");
-    }
+    console.log("")
+    console.log(data, "State данные")
+    console.log(tasks["16march2024"], "Первоначальные данные")
 
-    const changeData = (actionType: string, id: number, title: string) => {
+    const qunatityCompletedTask = action.getQuantityCompletedTasks();
+
+    // const changeActionInput = (checked: boolean) => {
+    //     setActionInput(checked ? "search" : "add");
+    // }
+
+    const filteredTasks = useMemo(() => {
+            if (typeTasks === "All") return data; 
+            const condition = typeTasks === "Completed";
+
+            return data.filter((item: ITask) => item.isCompleted === condition)
+    }, [typeTasks, data])
+
+    const changeData = useCallback(({ actionType, id, title }: {
+        actionType: string;
+        id?: number;
+        title?: string;
+      }) => {
         switch(actionType) {
             case "delete":
-                setData(action.delete(data, id, title));
+                if (id !== undefined && title !== undefined) {
+                    setData(action.delete(id, title));
+                } return;
+
+            case "status":
+                if (id !== undefined) {
+                    setData(action.changeActiveStatus(id));
+                } return;
+
+            case "clearCompleted":
+                setData(action.clearCompleted(data));
                 return;
+
             default: return;
         }
-    }
+    }, [data])
 
-    const findTaskByTitle = () => {
-        if (inputValue?.length) {
-            setData(action.find(data, inputValue));
-        }
-        if (inputValue!.length <= 1) {
-            setData(tasks[currentDay]);
-        }
-    }
-
-    const onChangeInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let value = e.target.value
-        if (actionInput === "add") {
-            value = capitalize(value);
-        }
-        else {
-            findTaskByTitle();
-        }
-        setInputValue(value);
-    }
+    const changeFilterType = useCallback((type: TTypeCompletedTask): void => {
+        setTypeTasks(type);
+    }, [])
 
     const addTask = () => {
         if (!inputValue?.length) return;
@@ -82,36 +96,84 @@ const TodoList: React.FC = () => {
 
 
 
+
+    const SearchMemo = useMemo(() => {
+        // const findTaskByTitle = () => {
+        //     if (inputValue?.length) {
+        //         console.log(filteredTasks, inputValue)
+        //         setData(action.find(data, inputValue));
+        //     }
+        //     if (inputValue!.length <= 1) {
+        //         setData(tasks[currentDay]);
+        //     }
+        // }
+
+        const onChangeInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+            let value = e.target.value
+            if (actionInput === "add") {
+                value = capitalize(value);
+            }
+            // else {
+            //     findTaskByTitle();
+            // }
+            setInputValue(value);
+        }
+
+        const isAllowedAdding = typeTasks === "All" || actionInput === "search";
+
+        return (
+            <Search 
+                size="large" 
+                disabled={!isAllowedAdding}
+                placeholder={currentInputPayload.placeholder} 
+                enterButton={currentInputPayload.icon}
+                onSearch={actionInput === "add" ? addTask : () => {}}
+                onChange={onChangeInputValue}
+                value={inputValue}
+            />
+        )
+    }, [inputValue, typeTasks, actionInput])
+
+
+    const ContentMemo = useMemo(() => (
+        <Content style={{ overflowY: 'auto', height: '100%' }}>
+            {
+                filteredTasks.length ? filteredTasks.map((item: ITask) => (
+                    <TaskCard dataItem={item} changeData={changeData} key={item.id + item.title} />
+                )) :
+                <NotFound typeInput={actionInput} typeTab={typeTasks} textSearch={inputValue} />
+            }
+        </Content>
+    ), [filteredTasks]);
+
+
+    // const SwitcherMemo = useMemo(() => (
+    //     <Switch 
+    //         checkedChildren={"Search"} 
+    //         unCheckedChildren={"Add"} 
+    //         onChange={changeActionInput}
+    //     />
+    // ), []);
+
+    
     return (
         <div className={stl.todo_block}>
             <div className={stl.todo_container}>
                 <div className={stl.todo_adding_search}>
-                    <Search 
-                        size="large" 
-                        placeholder={currentInputPayload.placeholder} 
-                        enterButton={currentInputPayload.icon}
-                        onSearch={actionInput === "add" ? addTask : findTaskByTitle}
-                        onChange={onChangeInputValue}
-                        value={inputValue}
-                    />
-                    
-                    <div className={stl.todo_switch_container}>
-                        <Switch 
-                            checkedChildren={"Search"} 
-                            unCheckedChildren={"Add"} 
-                            onChange={changeActionInput}/>
-                    </div>
+                    {SearchMemo}
+                    {/* <div className={stl.todo_switch_container}>
+                        {SwitcherMemo}
+                    </div> */}
                 </div>
                 <div className={stl.todo_list}>
-                    <Content style={{ overflowY: 'auto', height: '100%' }}>
-                        {
-                            data.map((item: ITask) => (
-                                <TaskCard dataItem={item} changeData={changeData} />
-                            ))
-                        }
-                    </Content>
+                    {ContentMemo}
                 </div>
-                <PanelActionsTasks />
+                <PanelActionsTasks 
+                    quantityCompleted={qunatityCompletedTask.completed} 
+                    allQuantity={qunatityCompletedTask.all}
+                    changeFilterType={changeFilterType}
+                    changeData={changeData}
+                />
             </div>
         </div>
     )
