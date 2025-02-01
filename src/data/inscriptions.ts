8a68c691ae86f89dbd77315e1d0f4bb6e444d71b
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-01 15:07:04 +0300

    [mb-3] Подписи

--- src/data/inscriptions.ts
+++ src/data/inscriptions.ts
@@ -4,5 +4,18 @@ export const t = {
         showInfo: "Show task information",
         moveUp: "Move higher",
         moveDown: "Move below",
+    },
+    buttons: {
+        deleteCompleted: "Clear completed",
+        all: "All",
+        active: "Active",
+        completed: "Completed",
+    },
+    messages: {
+        successfullyDeleted: "The task was successfully deleted",
+    },
+    questions: {
+        deleteTaskQues: "Are you sure to delete this task?"
     }
 };
+
