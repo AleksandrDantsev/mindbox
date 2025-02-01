9f7e784a30a0bf5c41ca2c88d47c15e956d83932
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-01 15:08:50 +0300

    [mb-3] Типы

---
+++ src/types/TTooltip.ts
@@ -0,0 +1,16 @@
+export type TTooltipPlacement = (
+    'top' 
+    | 'left' 
+    | 'right' 
+    | 'bottom' 
+    | 'topLeft' 
+    | 'topRight' 
+    | 'bottomLeft' 
+    | 'bottomRight' 
+    | 'leftTop' 
+    | 'leftBottom' 
+    | 'rightTop' 
+    | 'rightBottom'
+);
+
+
