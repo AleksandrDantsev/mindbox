83cbb45eed1188bd9f72fae3fbc9e0616b654520
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-01 15:08:13 +0300

    [mb-3] Рефакторинг

--- src/helpers/taskActions.ts
+++ src/helpers/taskActions.ts
@@ -1,51 +1,91 @@
 import { ITask } from "../types/TTasks"
 import { getMaxId } from "../utils/getMaxId";
+import { tasks, createTask } from "../data/tasks";
+import { appSetting } from "../appSettings";
 
 type TAction = {
-    delete: (data: ITask[], id: number, title: string) => ITask[];
+    delete: (id: number, title: string) => ITask[];
     find: (data: ITask[], title: string) => ITask[];
     add: (data: ITask[], title: string) => ITask[];
+    getQuantityCompletedTasks: () => {all: number, completed: number};
+    changeActiveStatus: (id: number) => ITask[];
+    clearCompleted: (data: ITask[]) => ITask[];
 }
 
-const template = (title: string, id: number): ITask => (
-    {
-        id,
-        title,
-        description: "Нужно прочитать 20 страниц",
-        isCompleted: false,
-        timeStart: "13:42", // todo
-        timeEnd: "16:12",
-        timeOfCreation: "12:11",
-        subtasks: [
-            {
-                subId: 1,
-                subTitle: "10 страниц на английском",
-                subIsCompleted: false
-            },
-            {
-                subId: 2,
-                subTitle: "10 страниц на немецком",
-                subIsCompleted: true
-            },
-        ]
-    }
-);
+const day = "16march2024"; // todo
+
+export const setActualData = (data: ITask[]): void => {
+    tasks[day] = data;
+    localStorage.setItem(appSetting.localStorageName, JSON.stringify(tasks));
+};
 
 
 export const action: TAction = {
-    delete: (data: ITask[], id: number, title: string) => {
-        return data.filter((item: ITask) => item?.id !== id && item?.title !== title);
+    delete: (id: number, title: string) => {
+        const result = tasks[day].filter((item: ITask) => item?.id !== id || item?.title !== title);
+        setActualData(result);
+        return result;
     },
+
     find: (data: ITask[], title: string) => {
-        return data.filter((item: ITask) => item?.title.indexOf(title) !== -1);
+        return data.filter((item: ITask) => item?.title.toLowerCase().indexOf(title.toLowerCase()) !== -1);
     },
+
     add: (data: ITask[], title: string) => {
         const copyData = [...data];
         const newId = getMaxId(data) + 1;
         
         copyData.unshift(
-            template(title, newId)
+            createTask({
+                title,
+                id: newId,
+                description: "",
+                isCompleted: false,
+                timeStart: "",
+                timeEnd: "",
+                timeOfCreation: "",
+                subtasks: [
+                {
+                    subId: 1,
+                    subTitle: "",
+                    subIsCompleted: false
+                },
+                {
+                    subId: 2,
+                    subTitle: "",
+                    subIsCompleted: true
+                },
+            ]
+            })
         );
+        setActualData(copyData);
         return copyData;
     },
+
+    changeActiveStatus: (id: number) => {
+        const result = tasks[day].map((item: ITask) => {
+            if (item?.id === id) item.isCompleted = !item.isCompleted;
+            return item;
+        })
+        setActualData(result);
+        return result
+    },
+
+    clearCompleted: (data: ITask[]) => {
+        const result = data.filter((item: ITask) => !item.isCompleted);
+        setActualData(result);
+        return result;
+    },
+
+    getQuantityCompletedTasks: () => {
+        const quantity = {
+            all: tasks[day].length,
+            completed: 0,
+        };
+        tasks[day].forEach((item: ITask) => {
+            if (item?.isCompleted) quantity.completed++;
+        })
+        return quantity;
+    },
+
 }
