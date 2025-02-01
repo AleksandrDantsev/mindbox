af324aaf8a90c48e0abbb150474ac9129a206b83
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-01 15:09:40 +0300

    [mb-3] utils

---
+++ src/utils/debounce.ts
@@ -0,0 +1,13 @@
+export function debounce<T extends (...args: any[]) => void>(fn: T, delay: number): (...args: Parameters<T>) => void {
+    let timeoutId: NodeJS.Timeout | null = null;
+
+    return function (...args: Parameters<T>) {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        timeoutId = setTimeout(() => {
+            fn(...args);
+        }, delay);
+    };
+}
