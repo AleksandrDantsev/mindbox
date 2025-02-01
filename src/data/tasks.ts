158edfd00202472e15e2937e40d9564751cb799a
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-01 15:07:24 +0300

    [mb-3] Генератор тасок

--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -1,10 +1,36 @@
-import { ITasks } from "../types/TTasks";
+import { ITasks, ITask } from "../types/TTasks";
+import { appSetting } from "../appSettings";
 
-export const tasks: ITasks = {
+
+export const createTask = ({
+    id,
+    title,
+    description = "",
+    isCompleted = false,
+    timeStart = "",
+    timeEnd = "",
+    timeOfCreation = "",
+    subtasks = [],
+    }: ITask
+): ITask => (
+    {
+        id,
+        title,
+        description,
+        isCompleted,
+        timeStart,
+        timeEnd,
+        timeOfCreation,
+        subtasks,
+    }
+);
+
+
+ let tasks: ITasks = {
     "16march2024": [
-        {
+        createTask({
             id: 1,
-            title: "Читать книгу",
+            title: "Прочитать книгу",
             description: "Нужно прочитать 20 страниц",
             isCompleted: false,
             timeStart: "13:42",
@@ -13,15 +39,69 @@ export const tasks: ITasks = {
             subtasks: [
                 {
                     subId: 1,
-                    subTitle: "10 страниц на английском",
+                    subTitle: "",
                     subIsCompleted: false
                 },
                 {
                     subId: 2,
-                    subTitle: "10 страниц на немецком",
+                    subTitle: "",
                     subIsCompleted: true
                 },
             ]
-        },
+        }),
+        createTask({
+            id: 2,
+            title: "Приготовить ужин",
+            description: "",
+            isCompleted: true,
+            timeStart: "13:42",
+            timeEnd: "16:12",
+            timeOfCreation: "12:11",
+            subtasks: [
+                {
+                    subId: 1,
+                    subTitle: "",
+                    subIsCompleted: false
+                },
+                {
+                    subId: 2,
+                    subTitle: "",
+                    subIsCompleted: true
+                },
+            ]
+        }),
+        createTask({
+            id: 3,
+            title: "Покормить кота",
+            description: "",
+            isCompleted: false,
+            timeStart: "13:42",
+            timeEnd: "16:12",
+            timeOfCreation: "12:11",
+            subtasks: [
+                {
+                    subId: 1,
+                    subTitle: "",
+                    subIsCompleted: false
+                },
+                {
+                    subId: 2,
+                    subTitle: "",
+                    subIsCompleted: true
+                },
+            ]
+        }),
     ],
 };
+
+
+const storedData = localStorage.getItem(appSetting.localStorageName);
+
+if (storedData) {
+    tasks = JSON.parse(storedData);
+}
+else {
+    localStorage.setItem(appSetting.localStorageName, JSON.stringify(tasks));
+}
+
+export { tasks };
