4fbb129c73e4770c0106dc7624285ca73fc2ca6d
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-01 15:06:23 +0300

    [mb-3] Рефакторинг

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,28 +1,27 @@
-import stl from "./TaskCard.module.scss";
-import { useState } from "react";
-import InfoTaskDrawer from "../InfoTask/InfoTask";
+import { useState, useCallback, memo } from "react";
+import type { TChangeData } from "../../types/ChangeDataAction";
 import { ITask } from "../../types/TTasks";
+import stl from "./TaskCard.module.scss";
+import InfoTaskDrawer from "../InfoTask/InfoTask";
 import TaskCardActions from "../TaskCardActions/TaskCardActions";
-
-
 import CompleteButton from "../../UI/CompleteButton/CompleteButton";
 
 interface ITaskCard {
     dataItem: ITask;
-    changeData: (actionType: string, id: number, title: string) => void;
+    changeData: TChangeData;
 }
 
 const TaskCard: React.FC<ITaskCard> = ({ dataItem, changeData }) => {
     const [isOpenedDrawer, setIsOpenedDrawer] = useState<boolean>(false);
 
-    const ch = (t) => {
-        
-        console.log(dataItem)
+    const changeStatus = () => {
+        changeData({actionType: "status", id: dataItem.id});
     };
 
-    const onOpenInfoTaskDrawer = () => {
+    const onOpenInfoTaskDrawer = useCallback(() => {
         setIsOpenedDrawer(true);
-    };
+    }, []);
+
     const onCloseInfoTaskDrawer = () => {
         setIsOpenedDrawer(false);
     };
@@ -33,8 +32,8 @@ const TaskCard: React.FC<ITaskCard> = ({ dataItem, changeData }) => {
                 <div className={stl.taskcard_complete_button}>
                     <CompleteButton 
                         isCompleted={dataItem.isCompleted} 
-                        onChange={ch}
-                        />
+                        onChange={changeStatus}
+                    />
                 </div>
                 <div className={stl.taskcard_title}>
                     <h3 className={dataItem.isCompleted ? stl.crossed_title : ""}>
@@ -46,17 +45,17 @@ const TaskCard: React.FC<ITaskCard> = ({ dataItem, changeData }) => {
                     changeData={changeData} 
                     onOpenInfoTaskDrawer={onOpenInfoTaskDrawer} 
                     />
-                <InfoTaskDrawer
-                    title={dataItem.title}
-                    size={"large"}
-                    isOpened={isOpenedDrawer}
-                    onClose={onCloseInfoTaskDrawer}
-                >
-                    <p>sdfsdf</p>
-                </InfoTaskDrawer>
             </div>
+            {/* <InfoTaskDrawer
+                title={dataItem.title}
+                size={"large"}
+                isOpened={isOpenedDrawer}
+                onClose={onCloseInfoTaskDrawer}
+            >
+                <p>sdfsdf</p>
+            </InfoTaskDrawer> */}
         </div>
     )
 }
 
-export default TaskCard;
+export default memo(TaskCard);
