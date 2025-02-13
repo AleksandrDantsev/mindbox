cdcc4765fc6e212d69a8319f72731aa1e69eadf3
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-13 08:55:57 +0300

    [mb-4] Добавлены поля в тип

--- src/types/ChangeDataAction.ts
+++ src/types/ChangeDataAction.ts
@@ -1,8 +1,13 @@
-export type TChangeData = ({ actionType, id, title, type }: {
+import { ITask } from "./TTasks";
+
+export type TChangeData = ({ actionType, id, title, type, placement, task, text }: {
     actionType: string;
     id?: number;
     title?: string;
     type?: TTypeCompletedTask;
+    placement?: "up" | "down";
+    task?: ITask;
+    text?: string;
   }) => void;
   
 export type TTypeCompletedTask = "Active" | "Completed" | "All";
