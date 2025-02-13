da48c47127f0121dd416457a53d917b783fda070
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-13 08:49:57 +0300

    [mb-4] Удален поиск

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -3,29 +3,21 @@ import { TTypeCompletedTask } from "../../types/ChangeDataAction";
 import stl from "./NotFound.module.scss";
 
 interface INotFound {
-    typeInput: "search" | "add";
     typeTab: TTypeCompletedTask;
     textSearch: string;
 }
 
-const NotFound: React.FC<INotFound> = ({ typeInput, typeTab, textSearch }) => {
+const NotFound: React.FC<INotFound> = ({ typeTab, textSearch }) => {
 
     const getDescription = useMemo(() => {
-        if (typeInput === "search") {
-            if (textSearch.length === 0) {
-                return "Try to find something";
-            }
-            return `No tasks with name "${textSearch}" were found`;
-        }
-        else {
-            const varTypesTasks = {
-                all: "Any",
-                active: "Active",
-                completed: "Completed",
-            };
-            return `${varTypesTasks[typeTab.toLowerCase() as keyof typeof varTypesTasks]} tasks were not found`;
-        }
-    }, [typeInput, typeTab, textSearch]);
+        const varTypesTasks = {
+            all: "Any",
+            active: "Active",
+            completed: "Completed",
+        };
+        return `${varTypesTasks[typeTab.toLowerCase() as keyof typeof varTypesTasks]} tasks were not found`;
+        
+    }, [typeTab, textSearch]);
 
     return (
         <div className={stl.not_found}>
