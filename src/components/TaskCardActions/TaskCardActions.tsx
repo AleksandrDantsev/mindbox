dfec96697047c401745fb8444bef2534cb07ade3
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-13 08:53:22 +0300

    [mb-4] Функция перемещения порядка задач + правки

--- src/components/TaskCardActions/TaskCardActions.tsx
+++ src/components/TaskCardActions/TaskCardActions.tsx
@@ -1,60 +1,85 @@
 import { memo } from "react";
 import stl from "./TaskCardActions.module.scss";
-import { Delete, ChevronSortUp, ChevronSortDown, OpenPanelRight } from "@ricons/carbon";
+import { Delete } from "@ricons/carbon";
 import ActionButton from "../../UI/ActionButton/ActionButton";
-import { Popconfirm, message } from 'antd';
+import { Popconfirm } from 'antd';
 import { ITask } from "../../types/TTasks";
 import { TChangeData } from "../../types/ChangeDataAction";
+import { ChevronDown, ChevronUp } from "@ricons/carbon";
+import { Information } from "@ricons/carbon";
 import { t } from "../../data/inscriptions";
 
 interface ITaskCardActions {
     dataItem: ITask;
     changeData: TChangeData;
     onOpenInfoTaskDrawer: () => void;
+    index: number | string;
+    isLastItemInList: boolean;
 }
 
 const tip = t.tooltip;
 
-const TaskCardActions: React.FC<ITaskCardActions> = ({ dataItem, changeData, onOpenInfoTaskDrawer }) => {
-    const [messageApi, contextHolder] = message.useMessage();
+const TaskCardActions: React.FC<ITaskCardActions> = ({ 
+    dataItem,
+    changeData,
+    index,
+    isLastItemInList,
+    onOpenInfoTaskDrawer 
+}) => {
 
-    const successDelete = () => {
-        messageApi.open({
-          type: 'success',
-          content: t.messages.successfullyDeleted,
+    const onDeleteTask = () => {
+        changeData({
+            actionType: "delete",
+            id: dataItem.id,
+            title: dataItem.title
         });
     };
 
-    const onDeleteTask = () => {
-        changeData({actionType: "delete", id: dataItem.id, title: dataItem.title});
-    };
+    const moveTask = (placement: "up" | "down") => {
+        changeData({
+            actionType: "moveTask",
+            task: dataItem, 
+            placement
+        })
+    }
 
     return (
         <div className={stl.taskcard_actions}>
-            {contextHolder}
             <ul>
-                {/* <li className={stl.taskcard_movers}>
-                    <ActionButton size={32} tooltipText={tip.moveUp}>
-                        <ChevronSortUp />
+                <li className={stl.taskcard_open_info}>       
+                    <ActionButton size={23} onClick={onOpenInfoTaskDrawer} tooltipText={tip.showInfo}>
+                        <Information style={{transform: "translateY(1px)"}}/> {/* TODO */}
                     </ActionButton>
-                    <ActionButton size={32} 
-                        onClick={() => {}} 
-                        tooltipText={tip.moveDown} 
-                        tooltipPlacement="bottom"
-                    >
-                        <ChevronSortDown />
-                    </ActionButton>
-                </li> */}
+                </li>
                 <li className={stl.taskcard_delete}>
-                    <ActionButton size={20} tooltipText={tip.deleteCard}>
+                    <ActionButton size={23} tooltipText={tip.deleteCard}>
                         <Popconfirm
                             title={tip.deleteCard}
                             description={t.questions.deleteTaskQues}
                             placement="top"
-                            onConfirm={() => {onDeleteTask(); successDelete()}}
-                            >
-                                <Delete />
+                            onConfirm={onDeleteTask}
+                        >
+                            <Delete />
                         </Popconfirm>
+                    </ActionButton>
+                </li>
+                <li className={stl.taskcard_movers}>
+                    <ActionButton 
+                        onClick={() => moveTask("up")}
+                        size={23} 
+                        tooltipText={tip.moveUp}
+                        visible={index !== 0}
+                    >
+                        <ChevronUp />
+                    </ActionButton>
+                    <ActionButton 
+                        size={23} 
+                        onClick={() => moveTask("down")} 
+                        tooltipText={tip.moveDown} 
+                        tooltipPlacement="bottom"
+                        visible={!isLastItemInList}
+                    >
+                        <ChevronDown />
                     </ActionButton>
                 </li>
             </ul>
@@ -62,9 +87,4 @@ const TaskCardActions: React.FC<ITaskCardActions> = ({ dataItem, changeData, onO
     )
 }
 
-                // <li className={stl.taskcard_open_info}>       
-                //     <ActionButton size={19} onClick={onOpenInfoTaskDrawer} tooltipText={tip.showInfo}>
-                //         <OpenPanelRight style={{transform: "translateY(2px)"}}/> {/* TODO */}
-                //     </ActionButton>
-                // </li>
 export default memo(TaskCardActions);
