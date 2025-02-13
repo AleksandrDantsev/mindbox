dcb6b4ed12fe2d027cef5987a57c5d7b90615014
Alexander Dantsev <123676006+AleksandrDantsev@users.noreply.github.com>
2025-02-13 08:53:38 +0300

    [mb-4] Добавлены поля

--- src/data/inscriptions.ts
+++ src/data/inscriptions.ts
@@ -3,19 +3,38 @@ export const t = {
         deleteCard: "Delete the task",
         showInfo: "Show task information",
         moveUp: "Move higher",
-        moveDown: "Move below",
+        moveDown: "Move below"
     },
     buttons: {
         deleteCompleted: "Clear completed",
         all: "All",
         active: "Active",
-        completed: "Completed",
+        completed: "Completed"
     },
     messages: {
         successfullyDeleted: "The task was successfully deleted",
+        successfullyChangedStatusActive: 'The status of the task was changed to "Active"',
+        successfullyChangedStatusCompleted: 'The status of the task was changed to "Completed"',
+        successfullyDeletedAllTasks : 'All completed tasks have been deleted',
+        successfullySetDescription : 'The task description has been saved'
     },
     questions: {
         deleteTaskQues: "Are you sure to delete this task?"
+    },
+    infoTitles: {
+        timeOfCreation: "Time of creation",
+        timeStart: "Time start",
+        timeEnd: "Start end",
+        status: "Status",
+        activeTask: "Active",
+        completedTask: "Completed",
+        descriptionTitle: "Description",
+        enterDescription: "Enter description",
+        allTasksAreCompleted: "All tasks are completed",
+        noTasks: "You don't have any tasks"
+    },
+    placeholders: {
+        addTask: "Add a task"
     }
 };
 
